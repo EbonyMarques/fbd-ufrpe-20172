4a8248d25a42ac21ecf1075d828f4f25e1dec9bb
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2018-02-15 12:27:22 -0300

    Corrigir erro de variáveis da sessão nulas

--- infraestrutura/auxiliar.php
+++ infraestrutura/auxiliar.php
@@ -1,5 +1,8 @@
 <?php
     session_start();
+    $_SESSION['alertaTipo'] = (isset($_SESSION['alertaTipo'])) ? $_SESSION['alertaTipo'] : '';
+    $_SESSION['alertaMensagem'] = (isset($_SESSION['alertaMensagem'])) ? $_SESSION['alertaMensagem'] : '';
+
     function conectar() {
         $conexao = mysqli_connect("localhost", "root", "", "fbd");
         mysqli_set_charset($conexao, 'utf8') or die (mysqli_error($conexao));
